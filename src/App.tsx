1331aadfe6c02014bb600e5ca3b5feb504b831af
Leonardo Imperatori <leonardo.imperatori12@gmail.com>
2021-11-29 12:33:59 -0300

    Adicionando Menu, Rodape com Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import NavBar from './components/estaticos/navbar/NavBar';
+import Footer from './components/estaticos/footer/Footer';
+import { Grid } from '@material-ui/core';
 import Home from './paginas/home/Home';
 import './App.css';
 
 
 function App() {
   return (
-    <Home/>
+    <>
+      <NavBar />
+        <Home />
+        <Footer />
+    </>
 
     
   );
