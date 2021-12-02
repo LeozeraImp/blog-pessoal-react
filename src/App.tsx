0d0cceb655440a339f8c34f29a4ceb617e2a108a
Leonardo Imperatori <leonardo.imperatori12@gmail.com>
2021-12-02 11:15:49 -0300

    Criando as Routers e Cadastro.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/estaticos/navbar/NavBar';
 import Footer from './components/estaticos/footer/Footer';
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
 import { Grid } from '@material-ui/core';
 import Home from './paginas/home/Home';
+import Login from "./paginas/Login/Login";
 import './App.css';
 
 
 function App() {
   return (
     <>
-      <NavBar />
-        <Home />
-        <Footer />
+        <Router>
+          <NavBar />
+            <Switch>
+              <div style = {{minHeight: "100vh" }}>
+
+              <Route exact path = "/">
+                  <Login />
+                </Route>
+
+              <Route path = "/login">
+                  <Login />
+                </Route>
+
+                <Route path = "/home">
+                  <Home />
+                </Route>
+
+                <Route path = "/cadastrousuario">
+                  <CadastroUsuario />
+                </Route>
+              </div>
+            </Switch>
+          <Footer />
+        </Router>
+
     </>
 
     
